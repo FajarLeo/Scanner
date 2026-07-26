acfb02970dfcabb1a9725a9d918fdee9aed55bf2
FajarLeo <fajarburex12@gmail.com>
2026-07-26 11:45:33 +0700

    Add API key to Pagespeed API request URL

--- api/scan.js
+++ api/scan.js
@@ -6,7 +6,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=desktop`;
+    const apiKey = process.env.PAGESPEED_API_KEY;
+    const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=desktop&key=${AIzaSyDjs5UFImkIBXkIKKBCP7Vd1YPwKuLCRt8}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     res.status(200).json(data);
