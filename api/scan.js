987fbdce19453cb2048e33faa24043340d6f8a58
FajarLeo <fajarburex12@gmail.com>
2026-07-26 11:48:29 +0700

    Fix API key usage in scan.js

--- api/scan.js
+++ api/scan.js
@@ -6,8 +6,9 @@ export default async function handler(req, res) {
   }
 
   try {
-    const apiKey = process.env.PAGESPEED_API_KEY;
-    const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=desktop&key=${AIzaSyDjs5UFImkIBXkIKKBCP7Vd1YPwKuLCRt8}`;
+    const apiKey = process.env.PAGESPEED_API_KEY; \
+    const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=desktop&key=${apiKey}`;
+    
     const response = await fetch(apiUrl);
     const data = await response.json();
     res.status(200).json(data);
