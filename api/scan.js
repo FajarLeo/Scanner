51e5dbd58e09dd94be6cfa52eb9f30697289543d
FajarLeo <fajarburex12@gmail.com>
2026-07-26 12:09:35 +0700

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const apiKey = process.env.PAGESPEED_API_KEY; \
+    const apiKey = process.env.PAGESPEED_API_KEYS; \
     const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=desktop&key=${apiKey}`;
     
     const response = await fetch(apiUrl);
